e5b582001570aea740487a6332bc38561ce521c7
Yu <e2296635@cmaisonneuve.qc.ca>
2024-03-04 13:54:13 -0500

    .

--- src/components/list-film/List-film.js
+++ src/components/list-film/List-film.js
@@ -1,64 +1,70 @@
-import './List-film.css';
+import './List-film.css'
+
+import { useEffect, useState } from 'react'
 
 function List() {
-  return (
-    <div class="content wrapper">
-      <h2>Liste des films</h2>
-      <ul>
-          <li>
-              <a href="#">
-                  <div class="pic"><img src="" alt=""/></div>
-                  <div class="txt">
-                      <h3>Queen Elizabeth II</h3>
-                      <p>This is a special collection in terms of Queen Elizabeth II.</p>
-                      <p><span>1125</span> items avaliable</p>
-                  </div>
-              </a>
-              <div class="promo">SPÉCIAL</div>
-          </li>
-          <li>
-              <a href="#">
-                  <div class="pic"><img src="" alt=""/></div>
-                  <div class="txt">
-                      <h3>Asian Collection</h3>
-                      <p>This is a special collection on asian stamps.</p>
-                      <p><span>1125</span> items avaliable</p>
-                  </div>
-              </a>
-          </li>
-          <li>
-              <a href="#">
-                  <div class="pic"><img src="" alt=""/></div>
-                  <div class="txt">
-                      <h3>Personal Collection of Dr Jhon Stampee</h3>
-                      <p>Personal collection of Dr Jhon Stampee.</p>
-                      <p>Limited time offer.</p>
-                  </div>
-              </a>
-          </li>
-          <li>
-              <a href="#">
-                  <div class="pic"><img src="" alt=""/></div>
-                  <div class="txt">
-                      <h3>A ride back to 1900s</h3>
-                      <p>Stamps used in 1900s from all over the world.</p>
-                      <p>Starting price: <span>$15</span></p>
-                  </div>
-              </a>
-          </li>
-          <li>
-              <a href="#">
-                  <div class="pic"><img src="" alt=""/></div>
-                  <div class="txt">
-                      <h3>A ride back to 1900s</h3>
-                      <p>Stamps used in 1900s from all over the world.</p>
-                      <p>Starting price: <span>$15</span></p>
-                  </div>
-              </a>
-          </li>
-      </ul>
-    </div>
-  );
+    const urlFilms = 'https://film-j3by.onrender.com/api/films'
+
+    const [ listeFilms, setListeFilms] = useState([])
+
+    return (
+        <div class="content wrapper">
+        <h2>Liste des films</h2>
+        <ul>
+            <li>
+                <a href="#">
+                    <div class="pic"><img src="" alt=""/></div>
+                    <div class="txt">
+                        <h3>Queen Elizabeth II</h3>
+                        <p>This is a special collection in terms of Queen Elizabeth II.</p>
+                        <p><span>1125</span> items avaliable</p>
+                    </div>
+                </a>
+                <div class="promo">SPÉCIAL</div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="pic"><img src="" alt=""/></div>
+                    <div class="txt">
+                        <h3>Asian Collection</h3>
+                        <p>This is a special collection on asian stamps.</p>
+                        <p><span>1125</span> items avaliable</p>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="pic"><img src="" alt=""/></div>
+                    <div class="txt">
+                        <h3>Personal Collection of Dr Jhon Stampee</h3>
+                        <p>Personal collection of Dr Jhon Stampee.</p>
+                        <p>Limited time offer.</p>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="pic"><img src="" alt=""/></div>
+                    <div class="txt">
+                        <h3>A ride back to 1900s</h3>
+                        <p>Stamps used in 1900s from all over the world.</p>
+                        <p>Starting price: <span>$15</span></p>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="pic"><img src="" alt=""/></div>
+                    <div class="txt">
+                        <h3>A ride back to 1900s</h3>
+                        <p>Stamps used in 1900s from all over the world.</p>
+                        <p>Starting price: <span>$15</span></p>
+                    </div>
+                </a>
+            </li>
+        </ul>
+        </div>
+    );
 }
 
 export default List

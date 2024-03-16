e82aa20e5f85d717191136c15108a44069e4edb6
yzl-2022 <119527280+yzl-2022@users.noreply.github.com>
2024-03-15 23:48:40 -0400

    Update Card-film.js

--- src/components/card-film/Card-film.js
+++ src/components/card-film/Card-film.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Card(props) {
   return (
     <Link to={`/film/${props.film.id}`}>
-        <div className="pic"><img src={`/img/${props.film.titreVignette}`} alt={`image pour ${props.film.titre}`}/></div>
+        <div className="pic"><img src={`img/${props.film.titreVignette}`} alt={`image pour ${props.film.titre}`}/></div>
         <div className="txt">
             <h3>{props.film.titre}</h3>
             <p className="description">{props.film.description && props.film.description.replace(/&#x27;/g, "'") }</p>
